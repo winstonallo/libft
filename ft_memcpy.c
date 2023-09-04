44cb9bc4361a518b957b7de5463de6c022b49189
Arthur Bied-Charreton <abied-ch@c3r2p6.42vienna.com>
2023-09-04 17:42:39 +0200

    gg

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,19 +6,27 @@
 /*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 17:21:27 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/09/04 17:28:57 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/09/04 17:42:18 by abied-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(void *dest, const void *src, unsigned long long n);
-{
-	unsigned char		*d = dest;
-    const unsigned char	*s = src;
+#include "libft.h"
 
+void	*ft_memcpy(void *dest, const void *src, unsigned long long n)
+{
+	const unsigned char	*s;
+	unsigned char		*d;
+	unsigned long long	i;
+
+	s = src;
 	i = 0;
+	d = dest;
+
 	while (i < n)
 	{
-		dest[i] = src[i];
+		d[i] = s[i];
 		i++;
 	}
+	return (d);
 }
+
