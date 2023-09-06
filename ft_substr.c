09a80d1cbd0705129bd0aafb6ff4bbbb9bfa3859
Arthur Bied-Charreton <abied-ch@c2r3p4.42vienna.com>
2023-09-06 11:07:18 +0200

    GG

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 10:45:57 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/09/06 11:00:54 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/09/06 11:05:43 by abied-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	x = 0;
 	dest = malloc((len + 1) * sizeof(char));
+	if (dest == NULL)
+		return (0);
 	while (x < len && s[start] != '\0')
 	{
 		dest[x] = s[start];
