e4438fa7e7ff00d1d063e9fac427dbc6cde3e40d
Arthur Bied-Charreton <abied-ch@c2r3p4.42vienna.com>
2023-09-06 16:43:19 +0200

    last function

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 13:42:04 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/09/06 16:36:32 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/09/06 16:39:34 by abied-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,5 +59,6 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
