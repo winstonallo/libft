2a816da0abbc650c4670f776d9a41f42c50762c7
Arthur Bied-Charreton <abied-ch@c2r3p2.42vienna.com>
2023-09-07 11:31:53 +0200

    fixed 0 input and last character edge case

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 11:06:23 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/09/06 12:45:34 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/09/07 11:31:22 by abied-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,19 @@ char	*ft_strrchr(const char *s, int c)
 
 	j = -1;
 	i = 0;
+	while (c > 256)
+		c -= 256;
 	while (s[i])
 	{
 		if (s[i] == c)
 			j = i;
 		i++;
 	}
+	printf("%d\n", j);
+	if (*s == c && c == 0)
+		return ((char *)s);
+	if (j == -1 && c == 0)
+		return ((char *)s + ft_strlen(s));
 	if (j == -1)
 		return (0);
 	return ((char *)s + j);
