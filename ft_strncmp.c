5ec7a136dd3deac98791fb8fdd4af4f66bfb30be
Arthur Bied-Charreton <abied-ch@c2r3p2.42vienna.com>
2023-09-07 11:58:04 +0200

    added check, fixed condition for while loop

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/05 11:16:54 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/09/06 12:46:27 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/09/07 11:57:27 by abied-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while ((s1[i] != '\0' || s2[i] != '\0') && i <= n)
+	if (n == 0)
+		return (0);
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
 		if (s1[i] != s2[i])
 		{
