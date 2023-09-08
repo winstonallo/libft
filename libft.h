76975ebe747a9e6102aa72dc6ba17b43253ca119
Arthur Bied-Charreton <abied-ch@c2r3p2.42vienna.com>
2023-09-08 14:53:52 +0200

    lstclear

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: abied-ch <abied-ch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 13:42:04 by abied-ch          #+#    #+#             */
-/*   Updated: 2023/09/08 14:41:10 by abied-ch         ###   ########.fr       */
+/*   Updated: 2023/09/08 14:48:58 by abied-ch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,5 +65,6 @@ int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
 
 #endif
