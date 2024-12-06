5066a6f6d0ce0002b12a4561cc2e3b42a9f05ff9
winstonallo <arthurbiedchar@gmail.com>
2024-12-06 10:22:32 +0100

    add: comments to str.c

--- str/str.c
+++ str/str.c
@@ -3,6 +3,11 @@
 #include "stdlib.h"
 #include <stdbool.h>
 
+// Returns the number of bytes between `str` and the next `\0` character.
+//
+// Safety:
+// It is the caller's responsiblity to ensure that `str` is a valid string terminated by a
+// `\0` character. Failure to do so will result in invalid memory access.
 size_t
 ft_strlen(const char *str) {
     size_t len;
@@ -16,6 +21,11 @@ ft_strlen(const char *str) {
     return len;
 }
 
+// Returns a heap-allocated duplicate of `str[start..start + len]`.
+//
+// Safety:
+// It is the caller's responsiblity to ensure that `str` is a valid string terminated by a
+// `\0` character. Failure to do so will result in invalid memory access.
 char *
 ft_substr(char const *str, unsigned int start, size_t len) {
     char *dest;
@@ -38,6 +48,12 @@ ft_substr(char const *str, unsigned int start, size_t len) {
     return dest;
 }
 
+// Allocates and returns a new string with characters specified in `set` removed
+// from the beginning and end of the input string `str`
+//
+// Safety:
+// It is the caller's responsiblity to ensure that `str` and `set` are valid strings terminated by
+// `\0` characters. Failure to do so will result in invalid memory access.
 char *
 ft_strtrim(char const *str, char const *set) {
     if (!str) {
@@ -74,16 +90,11 @@ ft_strtrim(char const *str, char const *set) {
     return res;
 }
 
-short
-ft_strncmp(const char *a, const char *b, size_t n) {
-    return ft_memcmp(a, b, n);
-}
-
-void
-ft_strncpy(char *dst, const char *src, size_t size) {
-    ft_memcpy(dst, src, size);
-}
-
+// Allocates and returns a string built by concatenating `a` and `b`.
+//
+// Safety:
+// It is the caller's responsiblity to ensure that `a` and `b` are valid strings terminated by
+// `\0` characters. Failure to do so will result in invalid memory access.
 char *
 ft_strjoin(char *a, char *b) {
 
@@ -140,6 +151,11 @@ count_words(const char *str, char delimiter) {
     return words;
 }
 
+// Allocates and returns an array of strings built by splitting `str` by `delimiter`.
+//
+// Safety:
+// It is the caller's responsiblity to ensure that `str` is a valid string terminated by a
+// `\0` character. Failure to do so will result in invalid memory access.
 char **
 ft_split(char const *str, char delimiter) {
     if (!str || !str[0]) {
--- str/str.h
+++ str/str.h
@@ -4,5 +4,9 @@
 #include <stddef.h>
 
 size_t ft_strlen(const char *str);
+char *ft_substr(char const *str, unsigned int start, size_t len);
+char *ft_strtrim(char const *str, char const *set);
+char *ft_strjoin(char *a, char *b);
+char **ft_split(char const *str, char delimiter);
 
 #endif
