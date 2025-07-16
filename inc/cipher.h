0ee0a6faa10dee96345b45e1e8aa19a6dfaa0d76
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2025-07-16 15:22:22 +0200

    Update README doc with cipher and digest utilities

--- inc/cipher.h
+++ inc/cipher.h
@@ -1,3 +1,5 @@
+// https://nvlpubs.nist.gov/nistpubs/legacy/sp/nistspecialpublication800-38d.pdf
+// https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.197-upd1.pdf
 
 #ifndef CIPHER_H
 #define CIPHER_H
@@ -41,10 +43,41 @@ typedef struct {
     uint8_t iv[12];
 } Aes256Gcm;
 
-int GCM_AD(Aes256Gcm *const C, Aes256Gcm *const P);
+// Performs AES-256-GCM authenticated encryption.
+// .
+// Encrypts the plaintext in `P->msg.data` and generates an authentication
+// tag. The resulting ciphertext and tag are stored in `C`.
+// .
+// Notes:
+// - The encryption may be performed in place by passing overlapping pointers
+// for `P` and `C`. This will overwrite the original plaintext (`P->msg.data`)!
+// - If the encrytion is not performed in place, `C->msg.data` and `C->aad.data`
+// must have `P->msg.len` and `P->aad.len` bytes allocated, respectively.
+// - The IV (`P->iv`) should be unique for each encryption with the same key.
+// - AAD (`P->aad.data`) is authenticated but not encrypted!
 void GCM_AE(Aes256Gcm *const P, Aes256Gcm *const C);
 
+// Performs AES-256-GCM authenticated decryption.
+// .
+// Decrypts the ciphertext in `C->msg.data` and verifies its authenticity using
+// the provided authentication tag (`C->tag`). The decrypted plaintext is stored
+// in `P->msg.data` (provided the authentication step passed).
+// .
+// The return value marks the status of the authentication step (0 for success,
+// non-zero for failure).
+// .
+// Notes:
+// - The decryption may be performed in place by passing overlapping pointers
+// for `C` and `P`. This will overwrite the original ciphertext (`C->msg.data`)!
+// - If the encrytion is not performed in place, `P->msg.data` and `P->aad.data`
+// must have, `C->msg.len` and `C->aad.len` bytes allocated, respectively.
+// - The function verifies authenticity before returning plaintext.
+// - If authentication fails, the content of `P->msg.data` is undefined.
+int GCM_AD(Aes256Gcm *const C, Aes256Gcm *const P);
+
+// Sets up the AES-256 context by expanding the provided key and storing the message.
 void AES256_Init(Aes256Data *data, const uint8_t key[AES256_KEY_SIZE_BYTES], const uint8_t *const msg, const size_t msg_len);
+
 uint8_t *InvCipher(uint8_t *in, uint8_t *const out, uint32_t *w);
 uint8_t *Cipher(const uint8_t *in, uint8_t *const out, uint32_t *w);
 uint32_t *KeyExpansion(const uint8_t key[AES256_KEY_SIZE_BYTES], uint32_t *const out);
